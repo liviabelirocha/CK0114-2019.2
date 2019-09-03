cf385611bf2fe61444f607bd7abf49a094dd6131
liviabelirocha <liviabelirocha@Outlook.com>
2019-09-02 21:59:23 -0300

    idk

--- exercicios/Aula 8/aula8-1.cpp
+++ exercicios/Aula 8/aula8-1.cpp
@@ -3,47 +3,71 @@
 
 using std::cout; using std::cin; using std::nothrow;
 
- //Situação inicial da pilha
-int size = 1;
-int topo = -1;
+template <typename T>
+struct Pilha {
+    T *v;
+    int ult, tam_v;
 
-int add (int *v, int num) {
-    return v[topo] = num;
-}
+    bool inicializar() {
+        v = new(nothrow) T [1];
+        if( v == nullptr ) return true;
+        ult = -1;
+        tam_v = 1;
+        return false;
+    }
 
-int remove (int *v) {
+    void terminar() { delete [] v; }
 
-}
+    bool vazia() {
+        return ( ult == -1 );
+    }
+
+    //Pré-condição: pilha não vazia
+    T topo() { return v[ult]; }
+
+    //Pré-condição: ult+1 <= tam_w 
+    bool redimensionar(int tam_w) {
+        T *w = new(nothrow) T [tam_w];
+        if ( w == nullptr ) return true;
+        for (int i; i <= ult; i += 1) w[i] = v[i];
+        return false;
+    }
+
+    bool empilhar(T e) {
+        if ( ult == tam_v - 1 ) {
+            if ( redimensionar(tam_v*2) ) return true;
+        }
+        ult += 1;
+        v[ult] = e;
+        return false;
+    }
+
+    //Pré-condição: pilha não vazia
+    bool desempilhar() {
+        ult -= 1;
+        if ( !vazia() && 4*(ult+1) <= tam_v ) {
+            if ( redimensionar(tam_v/2) ) return true;
+        }
+        delete [] v;
+        v = w;
+        tam_v = tam_w;
+        return false;
+    }
+};
 
 int main () {
-    int *v = new(nothrow) int [size];
-    if (v == NULL) return 1;
-
-    char c; int num;
-    do {
-        cout << "commands : add, remove, exit\n";
-        cin >> c;
-        switch (c){
-            case 'add':
-                if ( v[topo] == size ) {
-                    size = 2*size;
-                    int *newV = new(nothrow) int [size];
-                    if (v == NULL) return 1;
-
-                    for (int i = 0; i < size/2 ; i++) newV[i] = v[i];
-                    topo += 1;
-                }
-                cout << "number to add: "; cin >> num;
-                add(v, num);
-                break;
-
-            case 'remove':
-            break;
-        }
-
-    } while( c != 'exit' );
-
-    for (int i = 0; i < size; i++) cout << v[i] << ", "; 
-
-    delete [] v;
+    Pilha<double> pilha;
+    if ( pilha.inicializar() ) return 1;
+    char* command;
+    cout << "Entre o comando (adicionar, remover, sair): "; cin >> command;
+    while (command != "sair") {
+        if (command == "adicionar") {
+            double num;
+            cout << "Entre o número a ser adicionado: "; cin >> num;
+            if ( pilha.empilhar(num) ) return 2;
+        } else if ( command == "remover") {
+            if ( pilha.desempilhar() ) return 3;
+        } else cout << "Entre um comando válido";
+    }
+    pilha.terminar();
 }
